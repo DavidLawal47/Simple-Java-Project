dd0832eb38fe2ff9ad053fc9adaadb32b2e87698
David Lawal <agacecornish@yahoo.com>
2017-10-08 21:35:55 -0400

    initial commit

---
+++ src/com/simple/main/Main.java
@@ -0,0 +1,12 @@
+package com.simple.main;
+/*
+ *Sunday October 8 2017 9:10PM, Purpose; test programs and see proof of concepts "Test Dummy Crash Course"
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO Git!!!");
+
+	}
+
+}
